2ed279168ae27e293f99aecf4e8089de5e583313
chef0111 <letrangiabao14@gmail.com>
2026-03-26 10:35:03 +0700

    feat(portfolio): add fusionista project

--- modules/portfolio/components/projects/project-item.tsx
+++ modules/portfolio/components/projects/project-item.tsx
@@ -51,8 +51,10 @@ export function ProjectItem({
             />
           </div>
         ) : (
-          <div className="border-muted-foreground/15 bg-muted text-muted-foreground ring-line ring-offset-background mx-4 flex size-8 shrink-0 items-center justify-center rounded-lg border ring-1 ring-offset-1 select-none">
-            <BoxIcon className="size-6" />
+          <div className="bg-background m-1.5 mr-0 flex items-center rounded-md border">
+            <div className="border-muted-foreground/15 bg-muted text-muted-foreground ring-line ring-offset-background mx-3 flex size-8 shrink-0 items-center justify-center rounded-lg border ring-1 ring-offset-1 select-none">
+              <BoxIcon className="size-5" />
+            </div>
           </div>
         )}
 
