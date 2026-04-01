3f35718846c130c02e803de77ca1c9be8ed6f82f
chef0111 <letrangiabao14@gmail.com>
2026-04-01 16:50:28 +0700

    feat(ui): adjust mobile nav position for better aesthetic

--- modules/header/site-header-nav.tsx
+++ modules/header/site-header-nav.tsx
@@ -43,7 +43,7 @@ export function SiteHeaderNav({ children }: { children?: React.ReactNode }) {
       <div className="from-background pointer-events-none fixed inset-x-0 bottom-0 z-50 h-[calc(--spacing(16)+env(safe-area-inset-bottom,0px))] bg-linear-to-t from-[calc(env(safe-area-inset-bottom,0%))] to-transparent sm:hidden" />
       <div
         className={cn(
-          'bg-popover ring-foreground/10 dark:ring-foreground/20 fixed bottom-[calc(--spacing(4)+env(safe-area-inset-bottom,0px))] left-1/2 z-50 flex w-fit -translate-x-1/2 items-center rounded-xl py-1 pr-1 pl-2.5 shadow-md ring sm:hidden',
+          'bg-popover ring-foreground/10 dark:ring-foreground/20 fixed bottom-[calc(--spacing(3)+env(safe-area-inset-bottom,0px))] left-1/2 z-50 flex w-fit -translate-x-1/2 items-center rounded-xl py-1 pr-1 pl-2.5 shadow-md ring sm:hidden',
           '*:data-[slot=command-menu-trigger]:min-w-24 *:data-[slot=command-menu-trigger]:gap-2 *:data-[slot=command-menu-trigger]:rounded-none *:data-[slot=command-menu-trigger]:border-none *:data-[slot=command-menu-trigger]:bg-transparent *:data-[slot=command-menu-trigger]:px-0 *:data-[slot=command-menu-trigger]:hover:bg-transparent *:data-[slot=command-menu-trigger]:active:scale-none'
         )}
       >
