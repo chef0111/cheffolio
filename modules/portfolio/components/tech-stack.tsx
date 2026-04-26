bfdc0d982a2bb97c3589f245ea717dd1e1ad824a
chef0111 <letrangiabao14@gmail.com>
2026-04-26 11:22:54 +0700

    style(ui): adjust tech-stack bottom decor icons positioning for pixel-perfect

--- modules/portfolio/components/tech-stack.tsx
+++ modules/portfolio/components/tech-stack.tsx
@@ -27,8 +27,8 @@ export function TechStack() {
       <PanelContent className="relative mx-auto">
         <DecorIcon className="size-4" position="top-left" />
         <DecorIcon className="size-4" position="top-right" />
-        <DecorIcon className="size-4" position="bottom-left" />
-        <DecorIcon className="size-4" position="bottom-right" />
+        <DecorIcon className="mb-px size-4" position="bottom-left" />
+        <DecorIcon className="mb-px size-4" position="bottom-right" />
 
         <ul className="grid grid-cols-[repeat(auto-fit,minmax(32px,max-content))] justify-center gap-4 select-none">
           {TECH_STACK.map((tech) => {
