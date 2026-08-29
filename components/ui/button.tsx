1cbb2bbbdf3182b7389aedde35d893ed2c819b3f
Gia Bảo <letrangiabao14@gmail.com>
2026-08-29 16:17:43 +0700

    fix(button): disable selection on button texts

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "relative group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:scale-95 sm:active:scale-97 data-[slot=command-menu-trigger]:active:text-foreground data-[slot=command-menu-trigger]:data-pressed:text-foreground disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 ",
+  "relative group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none selection:bg-transparent selection:text-inherit [-webkit-touch-callout:none] focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:scale-95 sm:active:scale-97 data-[slot=command-menu-trigger]:active:text-foreground data-[slot=command-menu-trigger]:data-pressed:text-foreground disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 ",
   {
     variants: {
       variant: {
