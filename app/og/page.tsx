5f8e50c031190278fb08645bd5e9e78edf193ec5
Gia Bảo <letrangiabao14@gmail.com>
2026-09-02 00:12:17 +0700

    chore(og): update og page

--- app/og/page.tsx
+++ app/og/page.tsx
@@ -13,12 +13,14 @@ export const metadata: Metadata = {
 export default function OgPage() {
   return (
     <div className="flex h-dvh w-full flex-col items-center justify-center">
-      <div className="flex h-screen w-[80vw] flex-col justify-center border-x md:w-2xl">
-        <PanelContent className="decor-dl flex h-80 w-full flex-col items-center justify-center gap-6 self-center p-0 **:data-[slot=panel-plus]:size-8 [&_[data-slot=panel-plus]_svg]:size-6">
+      <div className="flex h-screen w-[80vw] flex-col justify-center border-x md:w-2xl lg:w-4xl">
+        <PanelContent className="decor-dl flex h-80 w-full flex-col items-center justify-center gap-4 self-center p-0 **:data-[slot=panel-plus]:size-8 md:h-120 md:gap-8 [&_[data-slot=panel-plus]_svg]:size-6">
           <FullWidthDivider position="top" className="bg-border" />
           <FullWidthDivider position="bottom" className="bg-border" />
-          <BrandMark className="h-24 w-auto" />
-          <span className="font-pixel text-2xl tracking-tight">giabao.dev</span>
+          <BrandMark className="h-24 w-auto md:h-42" />
+          <span className="font-pixel text-2xl tracking-tight md:text-5xl">
+            giabao.dev
+          </span>
         </PanelContent>
       </div>
     </div>
